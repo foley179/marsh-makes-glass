7733793927d979777d0dcf573c1727c532cfc54f
Dan Marsh <dan.marsh@rubixx.co.uk>
2026-07-08 19:05:45 +0100

    Deploy fixes

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/marsh-makes-glass/', // Add this line to set the base path for GitHub Pages
 })
