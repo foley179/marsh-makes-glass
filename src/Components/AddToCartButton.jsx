e959cd2370e978d43b64c7ea20e37547d692b81f
foley179 <foley179@hotmail.co.uk>
2026-07-10 17:09:21 +0100

    Changed cart button to go to after adding

--- src/Components/AddToCartButton.jsx
+++ src/Components/AddToCartButton.jsx
@@ -1,16 +1,26 @@
+import { useNavigate } from "react-router-dom"
 import { useCart } from "../Contexts/CartContext"
 import "./AddToCartButton.css"
 
 function AddToCartButton({ product }) {
-  const { AddToCart } = useCart()
+  const navigate = useNavigate();
+  const { cartItems, AddToCart } = useCart();
 
+  const isInCart = cartItems.some((item) => item.id === product.id);
+
+  // If already exists in the cart, redirect to the cart, else add the item
   function HandleClick() {
-    AddToCart(product)
+    if (isInCart) {
+      navigate("/marsh-makes-glass/cart");
+      return;
+    }
+
+    AddToCart(product);
   }
 
   return (
-    <button className="add-to-cart-button" onClick={HandleClick}>
-      Add to Cart
+    <button className={`add-to-cart-button ${isInCart ? "added" : ""}`} onClick={HandleClick}>
+      {isInCart ? "Go to Cart" : "Add to Cart"}
     </button>
   )
 }
